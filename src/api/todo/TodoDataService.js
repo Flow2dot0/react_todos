79e727c6f790d6af1ee6b3a6d8dd79339fbb7f55
Florian Gustin <florian.gustin@edu.itescia.fr>
2020-09-25 10:09:22 +0200

    feat : ?

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
-import {API_URL} from "../../Constants";
+import {API_URL, JPA_API_URL} from "../../Constants";
 
 class TodoDataService {
 
-    retrieveAllTodos = (name) => axios.get(`${API_URL}/users/${name}/todos`);
+    retrieveAllTodos = (name) => axios.get(`${JPA_API_URL}/users/${name}/todos`);
 
-    retrieveTodo = (name, id) => axios.get(`${API_URL}/users/${name}/todos/${id}`);
+    retrieveTodo = (name, id) => axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`);
 
-    deleteTodo = (name, id) => axios.delete(`${API_URL}/users/${name}/todos/${id}`);
+    deleteTodo = (name, id) => axios.delete(`${JPA_API_URL}/users/${name}/todos/${id}`);
 
-    updateTodo = (name, id, todo) => axios.put(`${API_URL}/users/${name}/todos/${id}`, todo);
+    updateTodo = (name, id, todo) => axios.put(`${JPA_API_URL}/users/${name}/todos/${id}`, todo);
 
-    addTodo = (name, todo) => axios.post(`${API_URL}/users/${name}/todos`, todo);
+    addTodo = (name, todo) => axios.post(`${JPA_API_URL}/users/${name}/todos`, todo);
 }
 
 export default  new TodoDataService();
